d1d2315e4ba471eb081e65099f60c65d461bd1d3
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-02-17 15:06:07 +0200

    changed react bundler on vite

---
+++ packages/react-nft-checkout/vite.config.ts
@@ -0,0 +1,46 @@
+import react from '@vitejs/plugin-react'
+import * as fs from 'fs'
+import * as path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import dts from 'vite-plugin-dts'
+
+const appDirectory = fs.realpathSync(process.cwd())
+const resolveApp = (relative: string) => path.resolve(appDirectory, relative)
+const root = path.resolve(__dirname, resolveApp('src'))
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, `${root}/index.ts`),
+        formats: ['cjs', 'es'],
+        fileName: format => `react-nft-checkout.${format}.js`,
+      },
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: ['react', '@emotion/react'],
+        // output: {
+        //   // Provide global variables to use in the UMD build
+        //   // for externalized deps
+        //   globals: {},
+        // },
+      },
+      sourcemap: true,
+    },
+    publicDir: 'static',
+    plugins: [react(), tsconfigPaths(), dts()],
+    resolve: {
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+      dedupe: ['react'],
+      alias: {
+        '@': `${root}/`,
+      },
+    },
+  }
+})
