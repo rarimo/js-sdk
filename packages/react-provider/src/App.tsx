d3e459ade6410c8edb09c8aed93e9541d0027f61
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-03-13 22:54:30 +0200

    fixed lint warnings

--- packages/react-provider/src/App.tsx
+++ packages/react-provider/src/App.tsx
@@ -5,6 +5,7 @@ import { useProvider } from './hooks'
 function App() {
   const { provider, ...rest } = useProvider(MetamaskProvider)
 
+  // eslint-disable-next-line no-console
   console.log({ provider, ...rest })
 
   return <div className="App">React Provider</div>
