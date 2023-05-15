0afb32fac788f652759dd081cf1edb950092de1c
Semen Loktionov <loktionov.kh@gmail.com>
2023-05-15 17:01:57 +0300

    add nft checkout op status

--- packages/nft-checkout/src/types/operation-event-bus.ts
+++ packages/nft-checkout/src/types/operation-event-bus.ts
@@ -2,22 +2,22 @@ import { OperationEventBusEvents } from '@/enums'
 
 import type { BridgeChain } from './common'
 import type { Target } from './operation'
+import type { CheckoutOperationStatus } from './operation'
 
-export type OperationInitiatedEventPayload = {
+export type OperationEventPayload = {
   isInitiated: boolean
   chainFrom: BridgeChain
   target?: Target
+  status: CheckoutOperationStatus
 }
 
 export type OperationEventMap = {
-  [OperationEventBusEvents.Initiated]: {
-    isInitiated: boolean
-    chainFrom: BridgeChain
-    target?: Target
-  }
+  [OperationEventBusEvents.Initiated]: OperationEventPayload
+  [OperationEventBusEvents.StatusChanged]: OperationEventPayload
 }
 
 export interface OperationSubscriber {
-  onInitiated(cb: (e: OperationInitiatedEventPayload) => void): void
+  onInitiated(cb: (e: OperationEventPayload) => void): void
+  onStatusChanged(cb: (e: OperationEventPayload) => void): void
   clearHandlers(): void
 }
