cbba1380e587d3c6da231c4f2c7f49c9773e6725
Tim McMackin <timothymcmackin@yahoo.com>
2023-06-01 10:01:55 -0400

    Squashed commits for easier rebasing:
    pick e5fee35f Make package descriptions consistent
    s 1350b0db make descriptions consistent
    s 252afb68 Tools that other packages use to run token swaps.
    s f9363136 Don't use this package directly
    s aaaacd7c Fix spacing
    s 50beca0b Mark bridge package as internal
    s bfd649db Mark swap package as internal

--- packages/swap/src/swapper.ts
+++ packages/swap/src/swapper.ts
@@ -2,6 +2,10 @@ import type { IProvider } from '@rarimo/provider'
 
 import type { Swapper, SwapperCreateFn } from '@/types'
 
+/**
+ * @description Instead of using this function directly, run token swaps as transactions with the [@rarimo/nft-checkout]() package.
+ * For more information about swaps, see [Swapping tokens](https://docs.rarimo.com/overview/swapping-tokens) in the Rarimo documentation.
+ */
 export const createSwapper = (
   createFn: SwapperCreateFn,
   provider: IProvider,
