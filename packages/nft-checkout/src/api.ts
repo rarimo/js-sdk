d0aed322646ead48384d361faf1be829263953a2
Semen Loktionov <loktionov.kh@gmail.com>
2023-07-10 11:02:54 +0300

    fix checkout api base url

--- packages/nft-checkout/src/api.ts
+++ packages/nft-checkout/src/api.ts
@@ -1,7 +1,5 @@
 import { Fetcher } from '@distributedlab/fetcher'
 
 export const checkoutApi = new Fetcher({
-  baseUrl: 'http://localhost:3000',
-  referrerPolicy: 'no-referrer',
-  credentials: 'omit',
+  baseUrl: 'https://api.testnet.rarimo.com/checkout',
 })
