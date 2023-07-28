f65d171a19ba625db57011f81a557b83f95223f4
Semen Loktionov <loktionov.kh@gmail.com>
2023-07-28 16:39:11 +0300

    add shared deps

--- packages/client/src/utils/enum-from-json.ts
+++ packages/client/src/utils/enum-from-json.ts
@@ -4,10 +4,12 @@ import {
 } from '@/codec/cosmos/gov/v1beta1/gov'
 import { ProposalStatus, VoteOption } from '@/enums'
 
-export const proposalStatusFromJSON = (status: string): ProposalStatus => {
+export const proposalStatusFromJSON = (
+  status: string | number,
+): ProposalStatus => {
   return _proposalStatusFromJSON(status) as unknown as ProposalStatus
 }
 
-export const voteOptionFromJSON = (option: string): VoteOption => {
+export const voteOptionFromJSON = (option: string | number): VoteOption => {
   return _voteOptionFromJSON(option) as unknown as VoteOption
 }
