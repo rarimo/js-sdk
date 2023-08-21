ee12ebb0be4537613fda1a2597d96f7aad12a874
lukachi <lukachisama@gmail.com>
2023-08-21 13:56:09 +0300

    revert eslint changes and fix by forceConsistentCasingInFileNames=false

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json', './packages/*/tsconfig.json'],
     ecmaVersion: 'latest',
     sourceType: 'module',
     ecmaFeatures: {
--- packages/providers-solana/lint.js
+++
@@ -1,3 +0,0 @@
-const lint = require('../../scripts/lint.base');
-
-lint(__dirname)
